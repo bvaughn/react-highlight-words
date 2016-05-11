924001ebc3ad3e252afd0144d8c7071fff463b0d
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-05-11 13:49:02 -0700

    Fixed wrong library target name

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -9,7 +9,7 @@ module.exports = {
     path: 'dist',
     filename: '[name].js',
     libraryTarget: 'commonjs2',
-    library: 'redux-search'
+    library: 'react-highlight-words'
   },
   plugins: [
   ],
