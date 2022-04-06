38119bcb55556eeab3c7bee61ae0c180bb772016
Siarhei Startsau <Siarhei_Startsau@epam.com>
2022-04-07 00:27:17 +0200

    feat: provide basic support of React 18

---
+++ webpack.config.test.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'babel/polyfill',
+    './website/index.js'
+  ],
+  output: {
+    path: 'build',
+    filename: '/static/[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: './website/index.html'
+    }),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        // to temporarily suppress "ReactDOM.render is no longer supported in React 18" warnings
+        // should be removed after switching to createRoot
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: path.join(__dirname, 'node_modules')
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css?modules&importLoaders=1', 'cssnext'],
+        exclude: path.join(__dirname, 'node_modules')
+      }
+    ]
+  }
+}
--- website/index.js
+++ website/index.js
@@ -1,14 +1,12 @@
 /**
- * This is the entray point to the documentation/demo/test harness site for react-highlight-words.
+ * This is the entry point to the documentation/demo/test harness site for react-highlight-words.
  * This target is published to the root of the `gh-pages` branch.
  * @flow
  */
 import Application from './Application'
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 
-render(
-  <Application/>,
-  document.getElementById('root')
-)
+const root = createRoot(document.getElementById('root'))
+root.render(<Application/>)
